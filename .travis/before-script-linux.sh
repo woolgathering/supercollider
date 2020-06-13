9998317506eeb127950a7b5fc84831443c942ad8
brianlheim <self@brianlheim.com>
2020-06-13 12:27:24 -0500

    [skip appveyor] travis: add libscsynth builds

--- .travis/before-script-linux.sh
+++ .travis/before-script-linux.sh
@@ -8,6 +8,10 @@ if $USE_SYSLIBS; then
     EXTRA_CMAKE_FLAGS="-DSYSTEM_BOOST=ON -DSYSTEM_YAMLCPP=ON"
 fi
 
+if $SHARED_LIBSCSYNTH; then
+    EXTRA_CMAKE_FLAGS="-DLIBSCSYNTH=ON $EXTRA_CMAKE_FLAGS"
+fi
+
 if [[ "$CLANG_VERSION" =~ ^[456].0$ ]]; then
     echo "using clang with libstdc++ instead of libc++"
     EXTRA_CMAKE_FLAGS="-DSC_CLANG_USES_LIBSTDCPP=ON $EXTRA_CMAKE_FLAGS"
